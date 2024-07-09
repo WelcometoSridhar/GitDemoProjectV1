4e6ef7025f2667aca15980af845d14bd3e38fa73
Sridhar <welcometosridhar@gmail.com>
2024-07-10 01:18:18 +0530

    commited by sridhar

---
+++ src/baseTest/baseTests.java
@@ -0,0 +1,13 @@
+package baseTest;
+
+public class baseTests {
+
+	public baseTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("this is base tests class");
+	}
+
+}
